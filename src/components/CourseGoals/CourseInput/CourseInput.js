184107089a7be8693be1bb764f3875eae38bd0eb
Patricia Nobre <2108le@gmail.com>
2021-07-26 17:29:33 -0300

    components and styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,27 +1,62 @@
 import React, { useState } from 'react';
-
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import './CourseInput.module.css';
+import styles from './CourseInput.module.css';
 
+// cria o componente CouseInput
 const CourseInput = props => {
+  
+  // cria o array para useSate
   const [enteredValue, setEnteredValue] = useState('');
 
+  // cria o array para useState
+  const [isValid, setIsValid] =  useState(true);
+
+  // função que usa o setEnteredValue 
   const goalInputChangeHandler = event => {
+
+    if (event.target.value.trim().length > 0) {
+
+      setIsValid(true);
+
+    }
+    
     setEnteredValue(event.target.value);
+
   };
 
+  // função que usa o setIsvalid
   const formSubmitHandler = event => {
+    
     event.preventDefault();
+    
+    if (enteredValue.trim().length === 0) {
+
+      setIsValid(false);
+
+      return;
+
+    }
+
     props.onAddGoal(enteredValue);
   };
 
+  // retorna o HTML/JSX
   return (
+    
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      
+      <div 
+      className={`${styles.['form-control']} ${!isValid && styles.invalid}`}>
+        
+        <label >Course Goal</label>
+        
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      
+        </div>
+
       <Button type="submit">Add Goal</Button>
+    
     </form>
   );
 };
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,13 +1,16 @@
 import React from 'react';
-
-import './Button.css';
+import styles from './Button.module.css';
 
 const Button = props => {
+
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
+
+      <button type={props.type} className={styles.button} onClick={props.onCLick}>
+        {props.children}
+      </button>
+
   );
+
 };
 
 export default Button;
